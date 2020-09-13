d1218bae4e21b5d64530e02181967e118f98e4c1
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-09-14 00:43:32 +0900

    makePJ

---
+++ myBank/myBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myBank
+//
+//  Created by 山根大生 on 2020/09/14.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
