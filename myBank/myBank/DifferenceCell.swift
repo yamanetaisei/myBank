9d228f6c77a754c9377208d6757e911d7da3aab1
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-09-14 16:01:44 +0900

    Cellのxibファイルを追加した

---
+++ myBank/myBank/DifferenceCell.swift
@@ -0,0 +1,24 @@
+//
+//  DifferenceCell.swift
+//  myBank
+//
+//  Created by 山根大生 on 2020/09/14.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class DifferenceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
