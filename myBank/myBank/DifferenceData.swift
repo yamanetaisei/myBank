a92b46a6f61fe36584ef6ac947a0b5b57be79f1b
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-09-15 16:22:49 +0900

    差分のデータベースモデルのファイルを作成

---
+++ myBank/myBank/DifferenceData.swift
@@ -0,0 +1,14 @@
+//
+//  DifferenceData.swift
+//  myBank
+//
+//  Created by 山根大生 on 2020/09/15.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class DifferenceData: Object {
+
+}
