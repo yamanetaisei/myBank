d77ce7d82f0181902c07362ac5e84b8a27aede34
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-09-15 15:31:22 +0900

    キーボードが閉じられる処理を追加

--- myBank/myBank/ChangeViewController.swift
+++ myBank/myBank/ChangeViewController.swift
@@ -30,6 +30,12 @@ class ChangeViewController: UIViewController, UITextFieldDelegate {
         incomeOrSpend.setTitle("支出", forSegmentAt: 1)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+    }
 
 }
