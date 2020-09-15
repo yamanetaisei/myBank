16938b2ec1b68b4cc0fa90faf475631ebde167e3
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-09-15 16:13:50 +0900

    総額を保管するデータベースのモデルを作成した

---
+++ myBank/myBank/TotalMoney.swift
@@ -0,0 +1,14 @@
+//
+//  TotalMoney.swift
+//  myBank
+//
+//  Created by 山根大生 on 2020/09/15.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TotalMoney: Object {
+    @objc dynamic var total: Int = 0
+}
